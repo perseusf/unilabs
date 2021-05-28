3b6e580eb870564af84931e1aa32fea4312a2da2
perseusf <71033927+perseusf@users.noreply.github.com>
2021-05-29 00:02:04 +0300

    delete cmake-debug

--- 2_semester/lab2/cmake-build-debug/test/lib/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/bzavo/Documents/01_LABS/labs/2_semester/lab2/test/lib/googlemock
-# Build directory: /mnt/c/Users/bzavo/Documents/01_LABS/labs/2_semester/lab2/cmake-build-debug/test/lib/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
